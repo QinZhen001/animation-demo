306d085495c91f7c75ca729c75d9dd8cb649dfa1
秦臻 <qinzhen@heywoods.cn>
2019-08-06 17:26:45 +0800

    改为webpack打包

--- bump-box/index.js
+++
@@ -1,7 +0,0 @@
-var inp = document.querySelector("input")
-var mod = document.querySelector(".module")
-
-
-inp.addEventListener("input", function () {
-    mod.style.setProperty("--notchSize", this.value + "px")
-})
