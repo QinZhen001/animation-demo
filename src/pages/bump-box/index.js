cb56576212e2b0833629580b77a97c7be6511fdd
秦臻 <qinzhen@heywoods.cn>
2019-08-06 18:20:26 +0800

    修复package.json问题

---
+++ src/pages/bump-box/index.js
@@ -0,0 +1,11 @@
+import "./index.html"
+import "./index.less"
+
+
+var inp = document.querySelector("input")
+var mod = document.querySelector(".module")
+
+
+inp.addEventListener("input", function () {
+  mod.style.setProperty("--notchSize", this.value + "px")
+})
