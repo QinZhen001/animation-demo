e944d078c44d6c0ddfe28e3dec310009c7832450
渣臻 <qz519189636@qq.com>
2020-05-30 19:06:46 +0800

    qz commit

--- src/pages/water-wave/index.js
+++ src/pages/water-wave/index.js
@@ -1,13 +1,37 @@
 import "./index.html"
-import "./index.less"
+import "./index.scss"
 
 
 
+let mx,my,timer
+let z = 2 
+
+console.log("$",$)
 
 
+$(".wave-wrapper3").on("click",(e)=>{
+  console.log("click",e)
+  mx =  e.pageX;
+  my = e.pageY;
+  z = z + 1;
+  wave(mx, my, z);
+})
 
 
-
+function wave(x,y,z){
+  $(".wave-wrapper3").prepend(
+     `
+     <div class="wave-body">
+        <div class="wave wave5"></div>
+        <div class="wave wave4"></div>
+        <div class="wave wave3"></div>
+        <div class="wave wave2"></div>
+        <div class="wave wave1"></div>
+        <div class="wave wave0"></div>
+      </div>
+     ` 
+  )
+}
 
 
 
