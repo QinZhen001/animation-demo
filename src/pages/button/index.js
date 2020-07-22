693280f0d78e10b8be3f7f747c3fd08cc8514683
渣臻 <qz519189636@qq.com>
2020-07-22 18:28:58 +0800

    qz commit

--- src/pages/button/index.js
+++ src/pages/button/index.js
@@ -1,17 +1,47 @@
-import "./index.html"
-import "./index.less"
+import "./index.html";
+import "./index.less";
 
-
-var myBtn = document.getElementsByTagName('button')[0]
-myBtn.addEventListener('mousemove', function (e) {
+var myBtn = document.getElementsByTagName("button")[0];
+myBtn.addEventListener("mousemove", function (e) {
   //计算相对于元素的位置 这里父元素是body所以可以用offsetLeft
   //因为button里面的span元素也会触发mousemove事件 所以要用currentTarget指向注册该事件的对象
-  const x = e.pageX - e.currentTarget.offsetLeft
-  const y = e.pageY - e.currentTarget.offsetTop
+  const x = e.pageX - e.currentTarget.offsetLeft;
+  const y = e.pageY - e.currentTarget.offsetTop;
 
-//        console.log('--x', `${ x }px`)
-//        console.log('--y', `${ y }px`)
+  //        console.log('--x', `${ x }px`)
+  //        console.log('--y', `${ y }px`)
 
-  e.currentTarget.style.setProperty('--x', `${ x }px`)
-  e.currentTarget.style.setProperty('--y', `${ y }px`)
-})
+  e.currentTarget.style.setProperty("--x", `${x}px`);
+  e.currentTarget.style.setProperty("--y", `${y}px`);
+});
+
+/**
+ * @author zhangxinxu(.com)
+ * @description 点击页面任意位置，标记坐标位置
+ */
+document.addEventListener("mousedown", (event) => {
+  var target = event.target;
+  var body = document.body;
+  var html = document.documentElement;
+
+  // 设置自定义属性
+  body.style.setProperty("--pagex", event.pageX);
+  body.style.setProperty("--pagey", event.pageY);
+  body.style.setProperty("--x", event.pageX);
+  body.style.setProperty("--y", event.pageY);
+
+
+  html.style.setProperty("--clientx", event.clientX);
+  html.style.setProperty("--clienty", event.clientY);
+
+  // pageXOffset 和 pageYOffset 属性相等于 scrollX 和 scrollY 属性。
+  html.style.setProperty("--scrolly", window.pageYOffset);
+
+  target.parentElement.style.setProperty("--target-width", target.clientWidth);
+  target.parentElement.style.setProperty(
+    "--target-height",
+    target.clientHeight
+  );
+  target.parentElement.style.setProperty("--target-left", target.offsetLeft);
+  target.parentElement.style.setProperty("--target-top", target.offsetTop);
+});
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,88 +1,98 @@
 // 首页
 
-import "./index.html"
-
+import "./index.html";
 
 // url拼接上.html
 const contents = [
   {
     title: "3d-page-turning 3d翻书效果",
-    url: "3d-page-turning"
-  }, {
+    url: "3d-page-turning",
+  },
+  {
     title: "bump-box 改变边框的盒子",
-    url: "bump-box"
-  }, {
+    url: "bump-box",
+  },
+  {
     title: "inverted-triangle 页面三角",
-    url: "inverted-triangle"
-  }, {
+    url: "inverted-triangle",
+  },
+  {
     title: "svg-path-volumn svg进度条",
-    url: "svg-path-volumn"
-  }, {
+    url: "svg-path-volumn",
+  },
+  {
     title: "seamless-rolling 轮播图",
-    url: "seamless-rolling"
-  }, {
+    url: "seamless-rolling",
+  },
+  {
     title: "clip-path svg遮罩",
-    url: ""
-  }, {
+    url: "clip-path",
+  },
+  {
     title: "fake-input-box 仿ofo输入框",
-    url: ""
-  }, {
+    url: "fake-input-box",
+  },
+  {
     title: "flash-banner 仿FLASH的图片轮换播放器",
-    url: ""
+    url: "flash-banner",
   },
   {
     title: "hand-code-svg 自己画svg",
-    url: ""
+    url: "hand-code-svg",
   },
   {
     title: "list-expend 列表展开效果",
-    url: ""
+    url: "list-expend",
   },
   {
     title: "button 按钮动画",
-    url: ""
+    url: "button",
   },
   {
     title: "demo 其他动画",
-    url: ""
+    url: "demo",
   },
   {
     title: "bootstrap-maizi 利用bootstrap库做的动画",
-    url: ""
+    url: "bootstrap-maizi",
   },
   {
     title: "loading 加载动画",
-    url: ""
+    url: "loading",
   },
   {
     title: "parallax 滚动视差",
-    url: ""
+    url: "parallax",
   },
   {
     title: "water-wave 水波效果",
-    url: ""
-  }
-]
+    url: "water-wave",
+  },
+  {
+    title: "滤镜效果",
+    url: "filter",
+  },
+];
 
-
-const body = document.body
+const body = document.body;
 // console.log("body", body)
 
-const ul = document.createElement("ul")
+const ul = document.createElement("ul");
 contents.sort((a, b) => {
-  const titleA = a.title
-  const titleB = b.title
-  return titleA.charCodeAt(0) - titleB.charCodeAt(0)
-})
-contents.forEach(item => {
-  const title = item.title
-  const url = title.split(" ")[0] + ".html"
-  const li = document.createElement("li")
-  const a = document.createElement("a")
-  a.innerText = title
-  a.setAttribute("href", url)
-  li.appendChild(a)
-  ul.appendChild(li)
-})
+  const titleA = a.title;
+  const titleB = b.title;
+  return titleA.charCodeAt(0) - titleB.charCodeAt(0);
+});
+contents.forEach((item) => {
+  const title = item.title;
+  let url = item.url ? item.url : title.split(" ")[0];
+  url += ".html"
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.innerText = title;
+  a.setAttribute("href", url);
+  li.appendChild(a);
+  ul.appendChild(li);
+});
 
-body.appendChild(ul)
+body.appendChild(ul);
